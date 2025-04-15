4c806edb40259aeebb840629dbde5b93994c3f41
Kaleemullah <159533186+kaleemullahahsan@users.noreply.github.com>
2025-04-15 15:37:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Tasks from "./Components/Tasks";
 import Tasksmine from "./Components/Tasksmine";
 
 function App() {
